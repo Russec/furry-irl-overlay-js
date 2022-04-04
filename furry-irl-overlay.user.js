dcc1dfadf9201a9c50831212f053eee46720570f
Russec <67121878+Russec@users.noreply.github.com>
2022-04-03 20:35:32 -0400

    Add files via upload

---
+++ furry-irl-overlay.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Furry_Irl Designs Overlay
+// @namespace    http://tampermonkey.net/
+// @version      2.1.8
+// @description  try to take over the canvas!
+// @author       oralekin, LittleEndu, seminal_sound
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @license      GPLv3
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+	window.addEventListener('load', () => {
+		document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+			(function () {
+				const i = document.createElement("img");
+				i.src = "https://i.imgur.com/97Horqu.png";
+				i.onload = () => {
+					if (i.width === i.height) {
+						i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+					} else {
+						i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+					}
+				};
+				return i;
+			})())
+	}, false);
+}
+
